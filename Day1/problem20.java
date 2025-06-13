b9d56438498f54fd52a54e19e498571cb8e1cc6e
Akshay <akshay.kumar1_cs22@gla.ac.in>
2025-06-13 10:51:10 +0530

    Initial commit

---
+++ Day1/problem20.java
@@ -0,0 +1,23 @@
+// CP - Create a program to swap two numbers
+// Hint =>
+// 1. Create a variable number1 and take user input.
+// 2. Create a variable number2 and take user input.
+// 3. Swap number1 and number2 and print the swapped output
+// I/P => number1, number2
+// O/P => The swapped numbers are ___ and ___
+
+
+import java.util.*;
+public class problem20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number1: ");
+        int number1 = input.nextInt();
+        System.out.print("Enter number2: ");
+        int number2 = input.nextInt();
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+        System.out.println("The swapped numbers are " + number1 + " and " + number2);
+    }
+}
---
+++ Day1/problem9.java
@@ -0,0 +1,14 @@
+//CP - Write a Program to compute the volume of Earth in km^3 and miles^3
+//Hint => Volume of a Sphere is (4/3) * pi * r^3 and radius of earth is 6378 km
+//O/P => The volume of earth in cubic kilometers is ____ and cubic miles is ____
+
+public class problem9 {
+    public static void main(String[] args) {
+        double radiusKm = 6378;
+        double volumeKm3 = (4.0 / 3) * Math.PI * Math.pow(radiusKm, 3);
+        double kmToMiles = 0.621371;
+        double radiusMiles = radiusKm * kmToMiles;
+        double volumeMiles3 = (4.0 / 3) * Math.PI * Math.pow(radiusMiles, 3);
+        System.out.println("The volume of earth in cubic kilometers is " + volumeKm3 + " and cubic miles is " + volumeMiles3);
+    }
+}
